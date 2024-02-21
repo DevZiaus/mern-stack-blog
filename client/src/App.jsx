8175fc68234b61dfe0bc53a2b6b457448822e928
DevZiaus <mzsbulbul@gmail.com>
2024-02-22 02:10:35 +0600

    Created Header Component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,11 +6,14 @@ import Projects from './pages/Projects'
 import SignIn from './pages/SignIn'
 import SignUp from './pages/SignUp'
 import Dashboard from './pages/Dashboard'
+import Header from './components/Header'
 
 
 export default function App() {
   return (
     <BrowserRouter>
+      <Header />
+      
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/about' element={<About />} />
