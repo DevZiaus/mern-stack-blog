0507e6139e64312493448604576ad6144cb500c5
DevZiaus <mzsbulbul@gmail.com>
2024-02-25 00:44:21 +0600

    Created Sign inAPI

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,4 +1,5 @@
 import bcryptjs from 'bcryptjs';
+import jwt from 'jsonwebtoken'
 import User from '../models/user.model.js'
 import { errorHandler } from '../utils/error.js';
 
@@ -22,5 +23,40 @@ export const signup = async (req, res, next) => {
     } catch (error) {
         next(error);
     }
-    
-}
+};
+
+export const signin = async (req, res, next) => {
+    const { email, password } =req.body;
+
+    if (!email || !password || email === '' || password === '') {
+        next(errorHandler(400, 'All fields are required'));
+    }
+
+    try {
+        const validUser = await User.findOne({ email });
+
+        if (!validUser) {
+            return next(errorHandler(404, 'Wrong Credentials'));
+        }
+        const validPassword = bcryptjs.compareSync(password, validUser.password);
+        
+        if (!validPassword) {
+            return next(errorHandler(400, 'Wrong Credentials'));
+        }
+
+        const token = jwt.sign(
+            { id: validUser._id},
+            process.env.JWT_SECRET,
+        );
+
+        const {password:pass, ...rest} = validUser._doc;
+
+        res.status(200)
+        .cookie('access_token', token, {
+            httpOnly: true,
+        })
+        .json(rest);
+    } catch (error) {
+        next(error);
+    }
+};
