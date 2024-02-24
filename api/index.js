88f7bb47397cfeaa28a9ccce75045cedfb9b07ec
DevZiaus <mzsbulbul@gmail.com>
2024-02-24 18:35:27 +0600

    Sign Up API Created

--- api/index.js
+++ api/index.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import userRoutes from './routes/user.route.js';
+import authRoutes from './routes/auth.route.js';
 
 dotenv.config();
 
@@ -13,7 +15,11 @@ mongoose.connect(DB).then(
 });
 
 const app = express();
+app.use(express.json());
 
 app.listen(3000, () => {
     console.log(`Server is running on port ${PORT}`);
-})
+});
+
+app.use('/api/user', userRoutes);
+app.use('/api/auth', authRoutes);
